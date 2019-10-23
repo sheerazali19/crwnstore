26e57e0a649c26a2ff72d4585601c37e141ed8ae
sheeraz ali <hacker.ascool@gmail.com>
2019-10-24 02:33:08 +0530

    'initial'

---
+++ src/homepage.comp.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>HATS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Shirts</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Pants</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Socks</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>Socks</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
